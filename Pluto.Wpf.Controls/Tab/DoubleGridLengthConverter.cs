fc666ab931eff72ab06cb020d2fd6dd3fc6a5506
Pluto Li <150735@163.com>
2024-11-12 21:40:05 +0800

    更新readme

--- Pluto.Wpf.Controls/Tab/DoubleGridLengthConverter.cs
+++ Pluto.Wpf.Controls/Tab/DoubleGridLengthConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 
 namespace Pluto.Wpf.Controls.Tab
 {
-    public class DoubleGridLengthConverter : IValueConverter
+    internal class DoubleGridLengthConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Pluto.Wpf.Controls
-自定义控件： 
-1. 异步置顶消息弹框
-2. 可关闭TabControl
+自用控件： 
+1. 异步置顶消息弹框：AsyncMessageBox
+2. 可关闭TabControl：CloseableTab
+
+
+例子：源码中的Test工程
